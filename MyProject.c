c5637249d4e3d1ed032780be4d884db57fdb64be
Tanto <kristianwiranata19@gmail.com>
2024-12-05 13:30:01 +0700

    get init

---
+++ MyProject.c
@@ -0,0 +1,18 @@
+/* PROGRAM ProgramName */
+/* An explanation of the program, which contains a brief description of the problem that will be resolved, including the input and output.*/
+
+/* DECLARATION: */
+/* all names that are used globally declared here */
+#include <stdio.h>
+
+/* procedure and function prototypes declared here */
+
+
+void main()
+{
+    /* DECLARATION: */
+    /* all names that are used locally declared here */
+    /* ALGORITHM: */
+    /* all program’s instructions are written here */
+}
+
